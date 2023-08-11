3252029219673d00fbaaeff52017ece0abc69431
Your Name <you@example.com>
2023-08-11 14:47:36 +0530

    Adedd Resume Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,30 @@ export default function Home() {
         </style>
       </head>
 
-      <main className='font-poppins'>
+      <main className="font-poppins bg-slate-600 min-h-screen px-10" >
         <section>
+
           {/* navigation */}
-          <nav>
-            <h1>Developed Milith De Silva</h1>
-            <ul>
-              {/* icon */}
+          <nav className="py-10 flex  justify-between ">
+            <h1 className=' text-sm'>Developed Milith De Silva</h1>
+            <ul className='flex items-center'>
+              {/*    icon */}
               <li>
-                <BsFillMoonFill/>
+                <BsFillMoonFill
+                  className="cursor-pointer text-xl mx-5 " />
+              </li>
+              <li>
+                <a
+                  href="https://drive.google.com/file/d/1dGEKvM5hEjMJ68GqL8NkzhT6qVRjk7zB/view?usp=sharing"
+
+                  download={"Reusme"}
+                  className=' bg-yellow-400 text-black px-4 py-2 border-none rounded-xl'>
+                  Resume</a>
+
               </li>
             </ul>
           </nav>
+
         </section>
       </main>
     </div>
